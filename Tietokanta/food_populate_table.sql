ca0daa6a53a26bc2c5f7e2cbd873ab499b4eac70
Ville Lehdonmäki <ville.lehdonkangas@tuni.fi>
2022-02-15 18:40:20 +0200

    Tietokanta

---
+++ Tietokanta/food_populate_table.sql
@@ -0,0 +1,308 @@
+INSERT INTO foods VALUES (
+    1, 'Ahven', 3, 4
+);
+
+INSERT INTO foods VALUES (
+    2, 'Amarantti', 2, 2
+);
+
+INSERT INTO foods VALUES (
+    3, 'Ananas', 5, 1
+);
+
+INSERT INTO foods VALUES (
+    4, 'Anjovis', 3, 4
+);
+
+INSERT INTO foods VALUES (
+    5, 'Ankerias', 3, 4
+);
+
+INSERT INTO foods VALUES (
+    6, 'Appelsiini', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    7, 'Aprikoosi', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    8, 'Auringonkukansiemen', 2, 2
+);
+
+INSERT INTO foods VALUES (
+    9, 'Avokado', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    10, 'Bambunverso', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    11, 'Banaani', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    12, 'Basilika', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    13, 'Bataatti', 5, 1
+);
+
+INSERT INTO foods VALUES (
+    14, 'Broileri', 4, 3
+);
+
+INSERT INTO foods VALUES (
+    15, 'Chiansiemen', 2, 2
+);
+
+INSERT INTO foods VALUES (
+    16, 'Chili', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    17, 'Etana', 6, 4
+);
+
+INSERT INTO foods VALUES (
+    18, 'Fenkoli', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    19, 'Granaattiomena', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    20, 'Greippi', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    21, 'Grillimakkara', 7, 5
+);
+
+INSERT INTO foods VALUES (
+    22, 'Guava', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    23, 'Haaparousku', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    24, 'Hampunsiemen', 2, 2
+);
+
+INSERT INTO foods VALUES (
+    25, 'Hauki', 3, 4
+);
+
+INSERT INTO foods VALUES (
+    26, 'Herkkusieni', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    27, 'Herkkutatti', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    28, 'Herne', 3, 1
+);
+
+INSERT INTO foods VALUES (
+    29, 'Herneenverso', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    30, 'Hirvenliha', 6, 7
+);
+
+INSERT INTO foods VALUES (
+    31, 'Hoki', 3, 4
+);
+
+INSERT INTO foods VALUES (
+    32, 'Hummeri', 3, 4
+);
+
+INSERT INTO foods VALUES (
+    33, 'Hunaja', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    34, 'Hunajameloni', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    35, 'Härkäpapu', 3, 1
+);
+
+INSERT INTO foods VALUES (
+    36, 'Inkivääri', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    37, 'Itu', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    38, 'Jauheliha', 6, 7
+);
+
+INSERT INTO foods VALUES (
+    39, 'Juolukka', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    40, 'Juuriselleri', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    41, 'Juusto', 4, 6
+);
+
+INSERT INTO foods VALUES (
+    42, 'Jänis', 6, 7
+);
+
+INSERT INTO foods VALUES (
+    43, 'Jääsalaatti', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    44, 'Kalkkuna', 4, 3
+);
+
+INSERT INTO foods VALUES (
+    45, 'Kampela', 3, 4
+);
+
+INSERT INTO foods VALUES (
+    46, 'Kana', 4, 3
+);
+
+INSERT INTO foods VALUES (
+    47, 'Kananmuna', 4, 3
+);
+
+INSERT INTO foods VALUES (
+    48, 'Kangasrousku', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    49, 'Kangastatti', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    50, 'Kani', 6, 7
+);
+
+INSERT INTO foods VALUES (
+    51, 'Kantarelli', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    52, 'Karpalo', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    53, 'Karvarousku', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    54, 'Karviainen', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    55, 'Kastanja', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    56, 'Katajanmarja', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    57, 'Katkarapu', 3, 4
+);
+
+INSERT INTO foods VALUES (
+    58, 'Kaura', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    59, 'Kaviaari', 3, 4
+);
+
+INSERT INTO foods VALUES (
+    60, 'Kesäkurpitsa', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    61, 'Kiinankaali', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    62, 'Kiivi', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    63, 'Kikherne', 3, 1
+);
+
+INSERT INTO foods VALUES (
+    64, 'Kilohaili', 3, 4
+);
+
+INSERT INTO foods VALUES (
+    65, 'Kirjolohi', 3, 4
+);
+
+INSERT INTO foods VALUES (
+    66, 'Kirsikka', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    67, 'Kookosmaito', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    68, 'Korianteri', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    69, 'Korvasieni', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    70, 'Kuha', 3, 4
+);
+
+INSERT INTO foods VALUES (
+    71, 'Kukkakaali', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    72, 'Kurkku', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    73, 'Kurpitsa', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    74, 'Kuskus', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    75, 'Kuusenkerkkä', 1, 1
+);
+
+INSERT INTO foods VALUES (
+    76, 'Kvinoa', 2, 2
+);
+
+INSERT INTO foods VALUES (
+    77, 'Kyssäkaali', 1, 1
+);
+
