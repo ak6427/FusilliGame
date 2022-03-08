79580bbbedb8cb650ac87bd6810b2b8b9a284c52
Juuso <juuso.kemppainen@tuni.fi>
2022-03-08 19:03:23 +0200

    Timer

--- Proto/Assets/Script/GameScore.cs
+++ Proto/Assets/Script/GameScore.cs
@@ -2,14 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+using System;
 
 public class GameScore : MonoBehaviour
 {
+    public float timeR = 300;
+    public TMP_Text R;
     [SerializeField] public int Score;
+    
     void Update()
     {
+        R.text = "Remaining "+ Math.Floor(timeR);   
         if(Score == 6){
             SceneManager.LoadScene("Victory");
         }    
+        
+        if(timeR > 0){
+            timeR -= Time.deltaTime;
+        }
+        else if(timeR <= 0){
+         SceneManager.LoadScene("Failure");
+        }
     }
 }
